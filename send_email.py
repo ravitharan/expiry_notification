4e505bead0192441512da2be90e14991305662a4
Ravi Ravitharan <ravitharan@gmail.com>
2021-10-02 09:27:48 +0100

    Add credential encription

--- send_email.py
+++ send_email.py
@@ -28,6 +28,7 @@ from email.encoders import encode_base64
 import base64
 import mimetypes
 import argparse
+import random
 
 
 def create_message(to, subject, message_text):
@@ -145,23 +146,53 @@ def make_googleapi_verification():
 
 
 def encrypt_credential():
-    """ convert credentials.json file into encrypted binary file credentials.bin
-    PROCEDURE:
-    1) Read all contents of credentials.json and save into a string variable.
-    2) Convert the string into bytes by encode()
-    3) Using xor operator convert each byte into another byte value.
-    4) Save the new bytes content into a binary file credentials.bin
-    """
-    pass
+    with open('credentials.json','r') as f:
+        data = f.read().encode()
+
+        keys = random.sample(range(256), 8)
+
+        encode_keys = base64.b64encode(bytes(keys))
+
+        hidden_keys = []
+        for k in encode_keys:
+            hidden_keys.append(k ^ b'k'[0])
+        
+        bin_data = []
+        for i, d in enumerate(data):
+            bin_data.append(d ^ keys[i%8])
+
+        #bin file
+        with open('credentials.bin','wb') as f:
+            key_len = len(hidden_keys)
+            f.write(key_len.to_bytes(1, 'little'))
+            f.write(bytes(hidden_keys))
+            f.write(bytes(bin_data))
 
 def decrypt_credential():
-    """ convert credentials.bin file into credentials.json file """
-    pass
+    with open('credentials.bin','rb') as f:
+        data = f.read()
+        key_len = data[0]
+        hidden_keys = data[1:key_len+1]
+
+        encode_keys = []
+        for k in hidden_keys:
+            encode_keys.append(k ^ b'k'[0])
+        encode_keys = bytes(encode_keys)
+
+        keys = base64.b64decode(encode_keys)
+        
+        credential = []
+        for i, d in enumerate(data[key_len+1:]):
+            credential.append(d ^ keys[i%8])
+        credential = bytes(credential)
+
+        with open('credentials.json', 'w') as f:
+             f.write(credential.decode())
 
 def send_email(address, title, message, attachment=''):
     decrypt_credential()
     service = make_googleapi_verification()
-    # os.remove('credentials.json')
+    os.remove('credentials.json')
     if attachment == '':
         message = create_message(address, title, message)
     else:
@@ -185,7 +216,7 @@ def email_main():
 if __name__ == '__main__':
     if os.path.exists('credentials.json'):
         encrypt_credential()
-        # os.remove('credentials.json')
+        os.remove('credentials.json')
     email_main()
 # [END gmail_quickstart]
 
